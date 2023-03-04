27e54d0946d5166d9ea7aeeb9ba6fcc7b5001144
Andry Pedak <andry.pedak@gmail.com>
2023-03-04 19:56:22 +0000

    Fix type

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-      base: dev ? '' : '/bolt-calculator ',
+      base: dev ? '' : '/bolt-calculator',
     }
 	}
 };
