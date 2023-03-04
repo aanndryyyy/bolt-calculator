721fef4bbc29d6f438b7f68d4e8f8c8426cf69b5
Andry Pedak <andry.pedak@gmail.com>
2023-03-04 20:23:30 +0000

    Fix SEO

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,9 +17,14 @@
   });
 </script>
 
-<main class="max-w-2xl mx-auto px-4 lg:px-0 mt-8 lg:mt-24">
-  <h1 class="text-3xl font-bold"><span class="text-green-600">Bolt</span> Calculator</h1>
-  <small class="text-gray-500">By <a href="http://pedak.me" target="_blank" rel="noreferrer">Andry Pedak</a></small>
+<svelte:head>
+  <title>Calculate Bolt Drive fare beforehand</title>
+  <meta name="description" content="Quickly see total trip cost before using Bolt Drive."/>
+</svelte:head>
+
+<main class="max-w-2xl mx-auto px-4 lg:px-0 mt-4 lg:mt-24">
+  <h1 class="text-3xl font-bold"><span class="text-green-600">Bolt Drive</span> Calculator</h1>
+  <small class="text-gray-500">By <a href="http://pedak.me" target="_blank" rel="noreferrer" class="hover:underline">Andry Pedak</a> | Prices don't match? <a href="https://github.com/aanndryyyy/bolt-calculator/blob/main/src/cars.js" target="_blank" rel="noreferrer" class="underline">Contribute</a></small>
 
   <hr class="mt-4 mb-6">
 
