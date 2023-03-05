8b99761f0f8f4071ed67767ae4e969db1ac5c500
Andry Pedak <andry.pedak@gmail.com>
2023-03-05 14:34:32 +0000

    Update cars

--- src/cars.ts
+++ src/cars.ts
@@ -2,30 +2,30 @@ import type { Car } from './Car';
  
 const cars: Car[] = [
   {
-    name: "VW T-Roc",
+    name: "Audi A1",
+    price: {
+      km:     0.24,
+      minute: 0.16,
+      hour:   7.99,
+      day:    30.90,
+    }
+  },
+  {
+    name: "Audi A3",
     price: {
       km:     0.25,
-      minute: 0.16,
-      hour:   6.99,
+      minute: 0.18,
+      hour:   7.99,
       day:    33.89,
     }
   },
   {
-    name: "Toyota Yaris",
-    price: {
-      km:     0.22,
-      minute: 0.13,
-      hour:   4.99,
-      day:    27.89,
-    }
-  },
-  {
-    name: "Skoda Kamiq",
+    name: "Audi e-tron",
     price: {
       km:     0.25,
-      minute: 0.16,
-      hour:   6.99,
-      day:    33.89,
+      minute: 0.69,
+      hour:   39.90,
+      day:    149.90,
     }
   },
   {
@@ -38,12 +38,165 @@ const cars: Car[] = [
     }
   },
   {
+    name: "Hyundai i20",
+    price: {
+      km:     0.24,
+      minute: 0.13,
+      hour:   4.99,
+      day:    27.90,
+    }
+  },
+  {
+    name: "Kia Picanto",
+    price: {
+      km:     0.22,
+      minute: 0.13,
+      hour:   4.99,
+      day:    27.89,
+    }
+  },
+  {
+    name: "Mercedes Sprinter",
+    price: {
+      km:     0.27,
+      minute: 0.15,
+      hour:   7.90,
+      day:    37.90,
+    }
+  },
+  {
+    name: "Toyota Corolla",
+    price: {
+      km:     0.25,
+      minute: 0.15,
+      hour:   6.29,
+      day:    29.89,
+    }
+  },
+  {
+    name: "Toyota C-HR",
+    price: {
+      km:     0.25,
+      minute: 0.18,
+      hour:   7.99,
+      day:    35.89,
+    }
+  },
+  {
+    name: "Toyota Land Cruiser",
+    price: {
+      km:     0.34,
+      minute: 0.49,
+      hour:   19.90,
+      day:    79.90,
+    }
+  },
+  {
     name: "Toyota RAV4",
     price: {
       km:     0.25,
       minute: 0.30,
       hour:   12.90,
       day:    43.90,
+    }
+  },
+  {
+    name: "Toyota Yaris",
+    price: {
+      km:     0.22,
+      minute: 0.13,
+      hour:   4.99,
+      day:    27.89,
+    }
+  },
+  {
+    name: "Toyota Yaris Cross",
+    price: {
+      km:     0.25,
+      minute: 0.18,
+      hour:   6.99,
+      day:    33.89,
+    }
+  },
+  {
+    name: "VW Arteon",
+    price: {
+      km:     0.25,
+      minute: 0.30,
+      hour:   12.90,
+      day:    43.90,
+    }
+  },
+  {
+    name: "VW e-Crafter",
+    price: {
+      km:     0.27,
+      minute: 0.13,
+      hour:   6.90,
+      day:    35.90,
+    }
+  },
+  {
+    name: "VW Crafter",
+    price: {
+      km:     0.27,
+      minute: 0.13,
+      hour:   6.90,
+      day:    35.90,
+    }
+  },
+  {
+    name: "VW T-Cross",
+    price: {
+      km:     0.25,
+      minute: 0.16,
+      hour:   6.99,
+      day:    33.89,
+    }
+  },
+  {
+    name: "VW T-Roc",
+    price: {
+      km:     0.25,
+      minute: 0.16,
+      hour:   6.99,
+      day:    33.89,
+    }
+  },
+  {
+    name: "VW T-Roc Cabrio",
+    price: {
+      km:     0.25,
+      minute: 0.16,
+      hour:   6.99,
+      day:    33.89,
+    }
+  },
+  {
+    name: "Škoda Scala",
+    price: {
+      km:     0.22,
+      minute: 0.13,
+      hour:   4.99,
+      day:    27.89,
+    }
+  },
+  {
+    name: "Škoda Fabia",
+    price: {
+      km:     0.24,
+      minute: 0.13,
+      hour:   4.99,
+      day:    27.90,
+    }
+  },
+  {
+    name: "Škoda Kamiq",
+    price: {
+      km:     0.25,
+      minute: 0.16,
+      hour:   6.99,
+      day:    33.89,
     }
   },
 ];
